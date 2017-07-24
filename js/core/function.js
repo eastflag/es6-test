decd49cd4f61cdc5b889b13cae87e3211c2963c9
eastflag <eastflag@gmail.com>
2017-07-24 10:50:00 +0900

    array test

---
+++ js/core/function.js
@@ -0,0 +1,10 @@
+//함수 선언문은 호이스팅 된다.
+myName("Yan", "Fan");
+
+/*var myName = function (first, last) {
+    console.log(first + last);
+}*/
+
+function myName(first, last) {
+    console.log(first + last);
+}
