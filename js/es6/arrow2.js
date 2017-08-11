17e3155ba203b28280ca3e143a1f96893c8b434f
eastflag <eastflag@gmail.com>
2017-08-11 15:20:02 +0900

    수정

--- js/es6/arrow2.js
+++ js/es6/arrow2.js
@@ -1,19 +1,27 @@
-var object1 = {
-    f1: () => {
-        console.log(this);
-        var f2 = () => { console.log(this); }
-        f2();
-        setTimeout(f2, 1000);
-    }
-};
-object1.f1();
+// arrow 펑션과 lexical function
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Functions/%EC%95%A0%EB%A1%9C%EC%9A%B0_%ED%8E%91%EC%85%98
+/*function Person() {
+    // Person() 생성자는 `this`를 자신의 인스턴스로 정의.
+    this.age = 0;
 
-var object2 = {
-    f1: function () {
-        console.log(this);
-        var f2 = () => { console.log(this); }
-        f2();
-        setTimeout(f2, 1000);
-    }
-};
-object2.f1();
+    setInterval(function growUp() {
+        // 비엄격 모드에서, growUp() 함수는 `this`를
+        // 전역 객체로 정의하고, 이는 Person() 생성자에
+        // 정의된 `this`와 다름.
+        this.age++;
+        console.log(this.age);
+    }, 1000);
+}
+
+var p = new Person();*/
+
+function Person(){
+    this.age = 0;
+
+    setInterval(() => {
+        this.age++; // |this| 는 정확히 person 객체를 참조
+        console.log(this.age);
+    }, 1000);
+}
+
+var p = new Person();
