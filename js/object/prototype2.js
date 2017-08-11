3a9bb9796994b06782a26b6886ee14c2490e49d3
eastflag <eastflag@gmail.com>
2017-08-11 19:36:01 +0900

    prototype 이해

--- js/object/prototype2.js
+++ js/object/prototype2.js
@@ -11,6 +11,8 @@ A.x=function() {
 var a = new A();
 console.log(a.x()); //hello
 console.log(a);
+// A는 함수 원형이고 a는 A로 부터 만들어진 객체이다.
+// a를 생성하기 위해서 위한 프로토타입은 hello이며 다른 값을 대입해도 변하지 않는다.
 
 // # 2.
 var B = function () { };
@@ -22,3 +24,4 @@ B.prototype.x = function () {
 };
 var b = new B();
 console.log(b.x());
+//B는 함수 원형이고 prototype에 x를 추가하면 함수원형이 변하게 된다.
